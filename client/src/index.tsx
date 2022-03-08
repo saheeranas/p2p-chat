acab26773d63f3401699adba12931f5f28339cdf
Saheer Anas <saheer09anas@gmail.com>
2022-03-08 17:07:06 +0530

    Add PWA functionality

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,3 +1,4 @@
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
@@ -16,3 +17,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorkerRegistration.register();
